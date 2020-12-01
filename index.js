a66b15170967d05d39444b98873e48d6103ae972
BaoDNguyen <54602191+BaoDNguyen@users.noreply.github.com>
2020-12-01 15:33:36 -0600

    test dialog sent mess

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 // will use this later to send requests
 const http = require('http');
 // import env variables
-require('dotenv').config();
+//require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const dialogflow_fulfillment = require('dialogflow-fulfillment');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -18,32 +20,51 @@ app.listen(port, () => {
     console.log(`🌏 Server is running at http://localhost:${port}`)
 });
 
-app.post('/weatherBot', (req, res) => {
+// app.post('/weatherBot', (req, res) => {
+//
+//     const weatherKey = '3f2b39ee96bea5d53296ae364ac222de';
+//     var cityName = 'London';
+//
+//     const reqUrl = encodeURI(
+//         `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${weatherKey}`
+//     );
+//
+//     http.get(
+//         reqUrl,
+//         (responseFromAPI) => {
+//
+//             let completeResponse = '';
+//             responseFromAPI.on('data', (chunk) => {
+//                 completeResponse += chunk;
+//             })
+//             responseFromAPI.on('end', () => {
+//                 try {
+//                     const weatherRespond = JSON.parse(completeResponse);
+//                     return res.json({weather: weatherRespond.list[0].weather[0].description});
+//                 } catch (e) {
+//                     console.error(e.message);
+//                 }
+//             });
+//         }).on('error',(e)=>{
+//             console.error('Got error: ${e.message}')
+//     });
+// });
 
-    const weatherKey = '3f2b39ee96bea5d53296ae364ac222de';
-    var cityName = 'London';
-
-    const reqUrl = encodeURI(
-        `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${weatherKey}`
-    );
-
-    http.get(
-        reqUrl,
-        (responseFromAPI) => {
-
-            let completeResponse = '';
-            responseFromAPI.on('data', (chunk) => {
-                completeResponse += chunk;
-            })
-            responseFromAPI.on('end', () => {
-                try {
-                    const weatherRespond = JSON.parse(completeResponse);
-                    return res.json({weather: weatherRespond.list[0].weather[0].description});
-                } catch (e) {
-                    console.error(e.message);
-                }
-            });
-        }).on('error',(e)=>{
-            console.error('Got error: ${e.message}')
+app.post('/weatherBot',(req,res)=>{
+    const agent = new dialogflow_fulfillment.WebhookClient({
+       request: req,
+       response: res,
     });
+
+    function greeting (agent) {
+        agent.add('hello from Bao!');
+    }
+
+    let intentMap = new Map();
+
+    intentMap.set('Default Welcome Intent',greeting);
+
+    agent.handleRequest(intentMap);
+
 });
+
