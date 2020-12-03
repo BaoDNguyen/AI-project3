f427e5f5b96f84202c467c268aa83f806d939781
BaoDNguyen <54602191+BaoDNguyen@users.noreply.github.com>
2020-12-02 22:59:20 -0600

    update

--- index.js
+++ index.js
@@ -35,146 +35,166 @@ app.post('/weatherBot', (req, res) => {
 const dialogflowFulfillment = (request, response) => {
     const agent = new WebhookClient({request,response});
 
-    let hasCityName = agent.parameters.city !== '' || history.city !== '';
-    let hasTimePoint1 = agent.parameters.date !== '' || agent.parameters['date-period'] !== '';
-    let hasTimePoint2 = agent.parameters.time !== '' || agent.parameters['time-period'] !== '';
-    let hasTimePoint = hasTimePoint1 && hasTimePoint2 || history.timePoint !== '';
-    let hasWeatherParameter = agent.parameters.weatherParameter[0] !== '' || history.weatherParameter !== '';
+    if (agent.intent === 'weatherRequest') {
+        let hasCityName = agent.parameters.city !== '' || history.city !== '';
+        let hasTimePoint1 = agent.parameters.date !== '' || agent.parameters['date-period'] !== '';
+        let hasTimePoint2 = agent.parameters.time !== '' || agent.parameters['time-period'] !== '';
+        let hasTimePoint = hasTimePoint1 && hasTimePoint2 || history.timePoint !== '';
+        let hasWeatherParameter = agent.parameters.weatherParameter[0] !== '' || history.weatherParameter !== '';
 
-    if (!hasCityName || !hasTimePoint || !hasWeatherParameter) {
+        if (!hasCityName || !hasTimePoint || !hasWeatherParameter) {
+            let botRes = management.botMessage(agent,history);
+            history = botRes.myHis;
+
+            let intentMap = new Map();
+            intentMap.set('agreement',addRes1);
+            intentMap.set('Default Welcome Intent',addRes1);
+            intentMap.set('disagreement',addRes1);
+            intentMap.set('weatherRequest',addRes1);
+
+            agent.handleRequest(intentMap);
+
+            function addRes1(agent) {
+                if (botRes.message !== '') agent.add(botRes.message);
+                else agent.add('Please tell me more!');
+            }
+
+        } else {
+            let cityName = (agent.parameters.city !== '') ? agent.parameters.city : history.city;
+            let timePoint = '';
+            if (hasTimePoint1 && hasTimePoint2) {
+                if (agent.parameters['date'] !== '') {
+                    timePoint += agent.parameters['date'].split('T')[0] + ' ';
+                    if (agent.parameters.time !== '') {
+                        let t = agent.parameters.time.split('T')[1].split('-')[0].split(':')[0];
+                        let tt = Math.round(+t/3)*3;
+                        if (tt === 24) tt = 0;
+                        timePoint += tt.toString()+':00:00';
+                    } else if (agent.parameters['time-period'] !== '') {
+                        let t = agent.parameters['time-period'].startTime.split('T')[1].split('-')[0].split(':')[0];
+                        let tt = Math.round(+t/3)*3;
+                        if (tt === 24) tt = 0;
+                        timePoint += tt.toString()+':00:00';
+                    }
+                } else if (agent.parameters['date-period'] !== '') {
+                    timePoint += agent.parameters['date-period'].startDate.split('T')[0]+' ';
+                    if (agent.parameters['time'] !== '') {
+                        let t = agent.parameters.time.split('T')[1].split('-')[0].split(':')[0];
+                        let tt = Math.round(+t/3)*3;
+                        if (tt === 24) tt = 0;
+                        timePoint += tt.toString()+':00:00';
+                    } else if (agent.parameters['time-period'] !== '') {
+                        let t = agent.parameters['time-period'].startTime.split('T')[1].split('-')[0].split(':')[0];
+                        let tt = Math.round(+t/3)*3;
+                        if (tt === 24) tt = 0;
+                        timePoint += tt.toString()+':00:00';
+                    }
+                }
+            } else timePoint = history.timePoint;
+            let weatherParameter = (agent.parameters.weatherParameter !== '') ? agent.parameters.weatherParameter[0] : history.weatherParameter;
+
+            const reqUrl = encodeURI(
+                `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${weatherKey}`
+            );
+
+            let data;
+
+            http.get(
+                reqUrl,
+                (responseFromAPI) => {
+
+                    let completeResponse = '';
+                    responseFromAPI.on('data', (chunk) => {
+                        completeResponse += chunk;
+                    })
+                    responseFromAPI.on('end', () => {
+                        try {
+                            const weatherRespond = JSON.parse(completeResponse);
+                            let N = weatherRespond.list.length;
+                            let count = -1;
+                            for (let i = 0; i < N; i++) {
+                                if (weatherRespond.list[i].dt_txt === timePoint) {
+                                    switch (weatherParameter) {
+                                        case 'temperature':
+                                            data = weatherRespond.list[i].main.temp;
+                                            break;
+                                        case 'maximum temperature':
+                                            data = weatherRespond.list[i].main.temp_max;
+                                            break;
+                                        case 'minimum temperature':
+                                            data = weatherRespond.list[i].main.temp_min;
+                                            break;
+                                        case 'humidity':
+                                            data = weatherRespond.list[i].main.humidity;
+                                            break;
+                                        case 'weather':
+                                            data = weatherRespond.list[i].weather[0].description;
+                                            break;
+                                    }
+                                    break;
+                                }
+                                count += 1;
+                            }
+                            if (count === N-1) data = 'no data';
+
+                            // console.log('data1',data);
+                            // console.log('url',reqUrl);
+                            // console.log('city',cityName);
+                            // console.log('time',timePoint);
+                            // console.log('weather',weatherParameter);
+
+                            let botRes = management.botMessage(agent,history,data);
+                            history = botRes.myHis;
+
+                            let intentMap = new Map();
+                            intentMap.set('agreement',addRes2);
+                            intentMap.set('Default Welcome Intent',addRes2);
+                            intentMap.set('disagreement',addRes2);
+                            intentMap.set('weatherRequest',addRes2);
+
+                            agent.handleRequest(intentMap);
+
+                            function addRes2(agent) {
+                                if (botRes.message !== '') agent.add(botRes.message);
+                                else agent.add('Please tell me more!');
+                            }
+
+                            // return response.json({weather: weatherRespond.list[0].weather[0].description});
+                        } catch (e) {
+                            console.error(e.message);
+                            return response.json({
+                                fulfillmentText: 'Could not get results at this time',
+                                source: 'weatherBot',
+                            })
+                        }
+                    });
+                }).on('error',(e)=>{
+                console.error('Got error: ${e.message}');
+                return response.json({
+                    fulfillmentText: 'Could not get results at this time',
+                    source: 'weatherBot',
+                })
+            });
+        }
+    } else {
         let botRes = management.botMessage(agent,history);
         history = botRes.myHis;
 
         let intentMap = new Map();
-        intentMap.set('agreement',addRes1);
-        intentMap.set('Default Welcome Intent',addRes1);
-        intentMap.set('disagreement',addRes1);
-        intentMap.set('weatherRequest',addRes1);
+        intentMap.set('agreement',addRes3);
+        intentMap.set('Default Welcome Intent',addRes3);
+        intentMap.set('disagreement',addRes3);
+        intentMap.set('weatherRequest',addRes3);
 
         agent.handleRequest(intentMap);
 
-        function addRes1(agent) {
+        function addRes3(agent) {
             if (botRes.message !== '') agent.add(botRes.message);
             else agent.add('Please tell me more!');
         }
-
-    } else {
-        let cityName = (agent.parameters.city !== '') ? agent.parameters.city : history.city;
-        let timePoint = '';
-        if (hasTimePoint1 && hasTimePoint2) {
-            if (agent.parameters['date'] !== '') {
-                timePoint += agent.parameters['date'].split('T')[0] + ' ';
-                if (agent.parameters.time !== '') {
-                    let t = agent.parameters.time.split('T')[1].split('-')[0].split(':')[0];
-                    let tt = Math.round(+t/3)*3;
-                    if (tt === 24) tt = 0;
-                    timePoint += tt.toString()+':00:00';
-                } else if (agent.parameters['time-period'] !== '') {
-                    let t = agent.parameters['time-period'].startTime.split('T')[1].split('-')[0].split(':')[0];
-                    let tt = Math.round(+t/3)*3;
-                    if (tt === 24) tt = 0;
-                    timePoint += tt.toString()+':00:00';
-                }
-            } else if (agent.parameters['date-period'] !== '') {
-                timePoint += agent.parameters['date-period'].startDate.split('T')[0]+' ';
-                if (agent.parameters['time'] !== '') {
-                    let t = agent.parameters.time.split('T')[1].split('-')[0].split(':')[0];
-                    let tt = Math.round(+t/3)*3;
-                    if (tt === 24) tt = 0;
-                    timePoint += tt.toString()+':00:00';
-                } else if (agent.parameters['time-period'] !== '') {
-                    let t = agent.parameters['time-period'].startTime.split('T')[1].split('-')[0].split(':')[0];
-                    let tt = Math.round(+t/3)*3;
-                    if (tt === 24) tt = 0;
-                    timePoint += tt.toString()+':00:00';
-                }
-            }
-        } else timePoint = history.timePoint;
-        let weatherParameter = (agent.parameters.weatherParameter !== '') ? agent.parameters.weatherParameter[0] : history.weatherParameter;
-
-        const reqUrl = encodeURI(
-            `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${weatherKey}`
-        );
-
-        let data;
-
-        http.get(
-            reqUrl,
-            (responseFromAPI) => {
-
-                let completeResponse = '';
-                responseFromAPI.on('data', (chunk) => {
-                    completeResponse += chunk;
-                })
-                responseFromAPI.on('end', () => {
-                    try {
-                        const weatherRespond = JSON.parse(completeResponse);
-                        let N = weatherRespond.list.length;
-                        let count = -1;
-                        for (let i = 0; i < N; i++) {
-                            if (weatherRespond.list[i].dt_txt === timePoint) {
-                                switch (weatherParameter) {
-                                    case 'temperature':
-                                        data = weatherRespond.list[i].main.temp;
-                                        break;
-                                    case 'maximum temperature':
-                                        data = weatherRespond.list[i].main.temp_max;
-                                        break;
-                                    case 'minimum temperature':
-                                        data = weatherRespond.list[i].main.temp_min;
-                                        break;
-                                    case 'humidity':
-                                        data = weatherRespond.list[i].main.humidity;
-                                        break;
-                                    case 'weather':
-                                        data = weatherRespond.list[i].weather[0].description;
-                                        break;
-                                }
-                                break;
-                            }
-                            count += 1;
-                        }
-                        if (count === N-1) data = 'no data';
-
-                        // console.log('data1',data);
-                        // console.log('url',reqUrl);
-                        // console.log('city',cityName);
-                        // console.log('time',timePoint);
-                        // console.log('weather',weatherParameter);
-
-                        let botRes = management.botMessage(agent,history,data);
-                        history = botRes.myHis;
-
-                        let intentMap = new Map();
-                        intentMap.set('agreement',addRes2);
-                        intentMap.set('Default Welcome Intent',addRes2);
-                        intentMap.set('disagreement',addRes2);
-                        intentMap.set('weatherRequest',addRes2);
-
-                        agent.handleRequest(intentMap);
-
-                        function addRes2(agent) {
-                            if (botRes.message !== '') agent.add(botRes.message);
-                            else agent.add('Please tell me more!');
-                        }
-
-                        // return response.json({weather: weatherRespond.list[0].weather[0].description});
-                    } catch (e) {
-                        console.error(e.message);
-                        return response.json({
-                            fulfillmentText: 'Could not get results at this time',
-                            source: 'weatherBot',
-                        })
-                    }
-                });
-            }).on('error',(e)=>{
-            console.error('Got error: ${e.message}');
-            return response.json({
-                fulfillmentText: 'Could not get results at this time',
-                source: 'weatherBot',
-            })
-        });
     }
+
+
 
 }
 
